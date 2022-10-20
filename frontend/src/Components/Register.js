5019d0780c4ffa90cef685680f5b1e501ec84469
frenchkebab <tubowangin@gmail.com>
2022-10-20 21:07:24 +0900

    Change Register Form

--- frontend/src/Components/Register.js
+++ frontend/src/Components/Register.js
@@ -1,52 +1,19 @@
 import { useRecoilState } from 'recoil';
-import { usernameState, passwordState, confirmPasswordState } from '../atoms';
+import { usernameState } from '../atoms';
 import { Link } from 'react-router-dom';
-import Axios from 'axios';
 
 function Register() {
   const [username, setUsername] = useRecoilState(usernameState);
-  const [password, setPassword] = useRecoilState(passwordState);
-  const [confirmPassword, setConfirmPassword] =
-    useRecoilState(confirmPasswordState);
 
   const onUsernameHandler = (e) => {
     setUsername(e.target.value);
   };
 
-  const onPasswordHandler = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const onConfirmPasswordHandler = (e) => {
-    setConfirmPassword(e.target.value);
-  };
-
   const onRegister = () => {
-    const userInfo = {
-      username,
-      password,
-    };
-
-    var hasEmptyInfo = false;
-
-    for (var i in userInfo) {
-      if (!userInfo[i]) hasEmptyInfo = true;
-    }
-
-    if (hasEmptyInfo) {
-      alert('모든 항목을 다 입력해주십시오.');
-    } else if (password !== confirmPassword) {
-      alert('비밀번호와 비밀번호확인이 일치하지 않습니다.');
+    if (!username) {
+      alert('Please fill in nickname');
     } else {
-      console.log(userInfo);
-      Axios.post('http://localhost:8000/register', userInfo)
-        .then((res) => {
-          console.log(res);
-          document.location.href = '/connect';
-        })
-        .catch((e) => {
-          console.error(e);
-        });
+      console.log(username);
     }
   };
 
@@ -55,13 +22,13 @@ function Register() {
       <section className="vh-100 gradient-custom body">
         <div className="container py-5 h-100">
           <div className="row d-flex justify-content-center align-items-center h-100">
-            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div className="col-12 col-md-7 col-lg-6 col-xl-5">
               <div className="card bg-white text-black">
-                <div className="card-body p-5 text-center">
+                <div className="card-body p-5 pb-0 text-center">
                   <div className="mb-md-5 mt-md-4 pb-5">
                     <h2 className="fw-bold mb-2 text-uppercase">Register</h2>
-                    <p className="text-gray-50 mb-5">
-                      Please enter your ID and password!
+                    <p className="text-gray-50 mt-3 mb-5">
+                      Please enter your nickname!
                     </p>
 
                     <div className="form-outline form-white mb-4">
@@ -76,44 +43,16 @@ function Register() {
                       />
                     </div>
 
-                    <div className="form-outline form-white mb-4">
-                      <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={onPasswordHandler}
-                        className="form-control form-control-lg"
-                      />
-                    </div>
-
-                    <div className="form-outline form-white mb-4">
-                      <input
-                        type="password"
-                        id="confirmPassword"
-                        name="confirmPassword"
-                        placeholder="Confirm Password"
-                        value={confirmPassword}
-                        onChange={onConfirmPasswordHandler}
-                        className="form-control form-control-lg"
-                      />
-                    </div>
                     <Link to="/connect">
                       <button
                         type="submit"
                         onClick={onRegister}
-                        className="btn bg-dark btn-outline-light btn-lg px-5"
+                        className="btn bg-dark btn-outline-light btn-lg px-5 mt-4"
                       >
                         Register
                       </button>
                     </Link>
                   </div>
-
-                  <p className="mb-0">Do you have account?</p>
-                  <Link to="/connect" className="text-black">
-                    Login!
-                  </Link>
                 </div>
               </div>
             </div>
