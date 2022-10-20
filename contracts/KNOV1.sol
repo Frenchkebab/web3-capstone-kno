ea48c39119ce18a7738ae21cd4f4a9ef9c5f4ff1
frenchkebab <tubowangin@gmail.com>
2022-10-20 15:00:00 +0900

    Adds contract and deployment code

--- contracts/KNOV1.sol
+++ contracts/KNOV1.sol
@@ -1,16 +1,54 @@
-// SPDX-License-Identifier
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
 
 contract KNOV1 {
+    address immutable admin;
+    uint256 private totalQuestion;
+
+    mapping(address => User) users;
+    mapping(address => Question[]) questionList;
+    mapping(address => Answer[]) answerList;
+    mapping(uint256 => Answer[]) posts;
+
     struct User {
-        address userwallet;
+        address userWalletAddress;
         string nickname;
         bool isRegistered;
     }
 
-    mapping(address => User) private users;
-
-    function isRegistered() external view returns (bool) {
-        return users[msg.sender].isRegistered;
+    struct Question {
+        address author;
+        uint256 qid;
+        string cid;
     }
+
+    struct Answer {
+        address author;
+        uint256 qid;
+        uint256 aid;
+        string cid;
+    }
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    function registerUser(string calldata nickname) external returns (bool) {
+        require(!users[msg.sender].isRegistered, "User already registered");
+        users[msg.sender] = User(msg.sender, nickname, true);
+        return true;
+    }
+
+    function signIn() external view returns (string memory) {
+        User memory user = users[msg.sender];
+        require(user.isRegistered, "User not registered");
+        return user.nickname;
+    }
+
+    function postQuestion(uint256 timestamp, string calldata cid)
+        external
+        returns (bool)
+    {}
+
+    function postAnswer(uint256 qid) external returns (bool) {}
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,28 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+const fs = require('fs');
+const hre = require('hardhat');
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const KNOV1 = await hre.ethers.getContractFactory('KNOV1');
+  const knoV1 = await KNOV1.deploy();
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  await knoV1.deployed();
 
-  await greeter.deployed();
+  console.log('KNOV1 deployed to:', knoV1.address);
 
-  console.log("Greeter deployed to:", greeter.address);
+  const data = {
+    KNOV1: knoV1.address,
+  };
+
+  fs.writeFile(
+    './frontend/src/artifacts/contracts/addresses.json',
+    JSON.stringify(data),
+    function (err) {
+      if (err) throw err;
+      console.log('Contract addresses saved successfully');
+    }
+  );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
